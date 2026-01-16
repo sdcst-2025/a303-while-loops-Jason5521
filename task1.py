8cbc2ff2aee2c33aba55b10ef45d3fc3c991362d
Jason5521 <jasonb185@deltalearns.ca>
2026-01-16 09:36:54 -0800

    _____

--- task1.py
+++ task1.py
@@ -25,3 +25,10 @@ Example:
 10
 ...
 """
+number = 0
+while number != 20:
+    number = number + 2
+    print(number)
+
+
+    
--- task2.py
+++ task2.py
@@ -29,3 +29,19 @@ Access granted
 
 
 """
+adminname = "admin"
+adminpassword = "12345"
+Loggedin = False
+
+while not Loggedin:
+    username = input("Enter username: ")
+    if username != adminname:
+        print("Access denied")
+    else:
+        password = (input("Enter password: "))
+        if password != adminpassword:
+            print("Access denied")
+        else:
+            Loggedin = True
+
+print("Access granted")
